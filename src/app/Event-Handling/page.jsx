5a916a4bb6feb9c1a9b248150beea917a9b614d0
Ajay740861 <palajay02817@gmail.com>
2024-10-10 17:27:28 +0530

    ..

--- src/app/Event-Handling/page.jsx
+++ src/app/Event-Handling/page.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 
 const EventHandling = () => {
     return (
-        <div className="w-[80%] mx-auto">
+        <div className="w-[80%] mx-auto border border-red-500"
+            onMouseMove={(e) => { console.log(e.clientX, e.clientY); }}
+        >
 
             <h1 className="text-center my-5 font-bold text=3xl">Event Handling</h1>
 
@@ -13,8 +15,20 @@ const EventHandling = () => {
             >Clicked Event</button>
 
             <input type="text"
-            onChange={() => { console.log('something changed');}}
-            className="border rounded p-3 mt-4 w-full" />
+                onChange={(e) => { console.log(e.target.value); }}
+                className="border rounded p-3 mt-4 w-full" />
+
+            <input type="color"
+                onChange={(e) => { document.body.style.backgroundColor = e.target.value; }}
+                className="mt-5" />
+
+            <input type="file"
+                onChange={(e) => { console.log(e.target.files); }}
+                multiple
+                className="block mt-5" />
+            <input type="text" onKeyDown={(e) => { console.log(e.code); }}
+                className="border rounded p-3 mt-4 w-full" />
+
         </div>
     )
 }
